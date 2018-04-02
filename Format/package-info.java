a6e9bceba9daeee9792813b832ec8a0fbc1b400c
Keenan21 <keenan.gaudio@ucalgary.ca>
2018-04-02 14:31:18 -0600

    Added comments for Fromat package

--- Format/package-info.java
+++ Format/package-info.java
@@ -1,8 +1,21 @@
 /**
+ * Contains the following:
+ * <dl>
+ * <dt>Communicate.java</dt>
+ * <dd>Contains information on the protocol used by this service</dd>
+ * <dt>Course.java</dt>
+ * <dd>Serializable, used to send data referring to entire courses between server/client</dd>
+ * <dt>Assignment.java</dt>
+ * <dt>Dropbox.java</dt>
+ * <dt>Submission.java</dt>
+ * <dt>ChatRoom.java</dt>
+ * <dt>Message.java</dt>
+ * </dl>
  * 
  */
 /**
  * @author keenangaudio, sunnynagam
- *
+ * @since April 1, 2018
+ * @version 1.0
  */
 package Format;
