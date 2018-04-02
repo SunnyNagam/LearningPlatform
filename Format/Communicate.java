9adc9ea65346b115aa66199354c3788d6ef1ed4f
Keenan21 <keenan.gaudio@ucalgary.ca>
2018-04-02 14:47:19 -0600

    Implemented Constants

--- Format/Communicate.java
+++ Format/Communicate.java
@@ -8,8 +8,23 @@ package Format;
  *
  */
 public interface Communicate {
-	static final int MIN_PORT = 4200;
-	static final int MAX_PORT = 4212;
+	// connection
+	static final int MIN_PORT 	= 4200;
+	static final int MAX_PORT 	= 4212;
 	
+	// commands
+	static final int REFRESH	= 0x43;
+	static final int LOGIN 		= 0x44;
+	static final int GET		= 0x45;
+	static final int SYNC		= 0x46;
+	
+	// types
+	static final int FILE		= 0x15;
+	static final int EMAIL		= 0x16;
+	static final int COURSE		= 0x20;
+	static final int ASSIGNMENT	= 0x21;
+	static final int SUBMISSION = 0x22;
+	static final int CHATROOM	= 0x23;
+	static final int MESSAGE	= 0x24;
 	
 }
