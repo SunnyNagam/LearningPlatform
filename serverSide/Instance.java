86777061edbf16fbd678421e856737be0c066526
Keenan21 <keenan.gaudio@ucalgary.ca>
2018-04-05 23:04:27 -0600

    replaced resultset sending with arraylists

--- serverSide/Instance.java
+++ serverSide/Instance.java
@@ -139,7 +139,7 @@ class Instance implements Runnable {
 			System.err.println("getting courses from db");
 			ResultSet r = helper.search(Communicate.COURSE, Communicate.PROFESSOR, String.valueOf(key) );
 			System.err.println("writing courses from db");
-			out.writeObject(r);
+			out.writeObject( parseRR(r) );
 			out.flush();
 		} catch (Exception e) {
 			try {
